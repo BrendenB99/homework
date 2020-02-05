d472e5e606a902bdd69a582d1f80a5eea3fabc2d
Brenden Benardin <brendenbenardin@gmail.com>
2020-02-04 23:28:48 -0600

    My Homework due on February 5th 2020

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const timeOfDay = "Morning";
+const greeting = `Good ${timeOfDay}!`;
+console.log(greeting);
+let nothing;
+console.log(nothing);
+console.log(typeof nothing);
+// we should expect the program to say false since nothing is there
+nothing = "";
+//I think we should use a string to notify that it was blank
+console.log(nothing);
+console.log(typeof nothing);
+//we should expect a string and nothing to print but a blank line
+console.log(brenden);
+//we should get false here since it doesnt exist
+console.log(greeting);
+const name = "Brenden";
+let favoriteSong = "Play -Alan Walker";
+let wage = 9.50;
+let age = 20;
+const onlyChild = false;
+let satisfied = false;
+let favoriteThing = "World of Warcraft";
+console.log(typeof favoriteThing);
+favoriteSong = "How far I'll go -Auli'i Cravalho"
+favoriteThing = 24;
+console.log(typeof favoriteThing);
+age = 21;
+wage = 10.50;
+satisfied = true;
+console.log("Hi my name is Brenden and one of my favorite song is " + favoriteSong);
